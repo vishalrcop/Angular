68c8667fe27df5bf2c4a492fc1e2c1a88af13c25
Vishalkumar <vishalrcop@gmail.com>
2019-03-16 21:00:40 -0400

    Promise Part 2
    Promise Part 2.
    Spinner is added.
    Spinner will be shown when data is loaded.
    In services, instead of returning resolved promise; A obeject of promise will return, in which there will be two option resolve(implemnted) and reject(not implemented).

--- conFusion/conFusion/src/app/app.module.ts
+++ conFusion/conFusion/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -63,6 +64,7 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     MatSlideToggleModule,
     MatSelectModule,
+    MatProgressSpinnerModule,
     ReactiveFormsModule   
   ],
   providers: [
