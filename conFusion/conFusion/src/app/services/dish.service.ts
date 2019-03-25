596663065b2cdc76b3a0bba7c1962143404d5dce
Vishalkumar <vishalrcop@gmail.com>
2019-03-25 17:43:52 -0400

    rxjs Part 2
    rxjs Part 2

--- conFusion/conFusion/src/app/services/dish.service.ts
+++ conFusion/conFusion/src/app/services/dish.service.ts
@@ -14,11 +14,16 @@ export class DishService {
   }
   
   getDish(id: string): Observable<Dish> {
-    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
+    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(0));
   }
 
   getFeaturedDish(): Observable<Dish> {
     return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
   }    
+
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
+  }
+
   constructor() { }
 }
