092b2a53d568911627df1e01faaad65a81a8c64c
Vishalkumar <vishalrcop@gmail.com>
2019-04-01 16:21:13 -0400

    Attribute Directives
    We can add Attributes using
     Attribute directives. Here we are adding attribute highlight using AppHighlight attribute directives.

--- conFusion/conFusion/src/app/app.module.ts
+++ conFusion/conFusion/src/app/app.module.ts
@@ -39,6 +39,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
